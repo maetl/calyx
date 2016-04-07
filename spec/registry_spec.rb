58794c7e1738fa427bc9b14febba1c3f8642ac18
Mark Rickerby <me@maetl.net>
2016-04-07 21:54:34 +1000

    Add notes about improving the representation of tree structures

--- spec/registry_spec.rb
+++ spec/registry_spec.rb
@@ -5,6 +5,17 @@ describe Calyx::Grammar::Registry do
     Calyx::Grammar::Registry.new
   end
 
+  context '#evaluate' do
+    # Need to do some more work to determine what the tree representation should look like.
+    #
+    # In particular, need to look at whether the :choice and :concat nodes could be simplified
+    # in cases where there is only a single production rather than multiple choices.
+    #
+    # The best way to resolve this is probably to try generating some interesting trees and
+    # seeing what is useful in practice.
+    it 'should return a tree representation of the grammar'
+  end
+
   specify 'registry evaluates the start rule' do
     registry.start('atom')
     expect(registry.evaluate).to eq([:start, [:choice, [:concat, [[:atom, "atom"]]]]])
