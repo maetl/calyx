a207b330f6bb7d364d4b6c96895a44ba24c2ca5e
Mark Rickerby <me@maetl.net>
2016-04-13 00:38:21 +1000

    Add additional tests of memoized symbols in template strings

--- spec/memo_spec.rb
+++ spec/memo_spec.rb
@@ -2,14 +2,22 @@ require 'spec_helper'
 
 describe 'memoized rules' do
   specify 'memoized symbol prefix' do
-    direction = Calyx::Grammar.new(123) do
+    grammar = Calyx::Grammar.new do
       rule :start, "Enter {character}. Exit {character}."
       rule :character, :@name
       rule :name, "Vladimir", "Estragon"
     end
 
-    10.times do
-      expect(direction.generate).to eq("Enter Vladimir. Exit Vladimir.")
+    expect(grammar.generate).to eq(grammar.generate)
+  end
+
+  specify 'memoized template expression' do
+    grammar = Calyx::Grammar.new do
+      rule :burger, "Monolith {@extras}"
+      rule :burger_combo, "Jumbo Monolith {@extras}"
+      rule :extras, "w/ extra grease", "w/ polycheese"
     end
+
+    expect(grammar.generate(:burger_combo)).to include(grammar.generate(:burger))
   end
 end
