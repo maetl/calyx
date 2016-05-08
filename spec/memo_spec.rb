e7f8488e2752e54ac51bf628a822d74fd1f0d63d
Mark Rickerby <me@maetl.net>
2016-05-08 23:52:13 +1000

    Clarify memoize specs

--- spec/memo_spec.rb
+++ spec/memo_spec.rb
@@ -3,21 +3,35 @@ require 'spec_helper'
 describe 'memoized rules' do
   specify 'memoized rule mapped with symbol prefix' do
     grammar = Calyx::Grammar.new do
-      rule :start, "Enter {character}. Exit {character}."
-      rule :character, :@name
-      rule :name, "Vladimir", "Estragon"
+      rule :start, '{@tramp}:{@tramp}'
+      rule :tramp, :@character
+      rule :character, 'Vladimir', 'Estragon'
     end
 
-    expect(grammar.generate).to eq(grammar.generate)
+    actual = grammar.generate.split(':')
+    expect(actual.first).to eq(actual.last)
   end
 
   specify 'memoized rule mapped with template expression' do
     grammar = Calyx::Grammar.new do
+      rule :start, :pupper
       rule :pupper, '{@spitz}:{@spitz}'
       rule :spitz, "pomeranian", "samoyed", "shiba inu"
     end
 
-    actual = grammar.generate(:pupper).split(':')
+    actual = grammar.generate.split(':')
     expect(actual.first).to eq(actual.last)
+  end
+
+  specify 'memoized rules change between multiple runs' do
+    grammar = Calyx::Grammar.new do
+      rule :start, '{flower}{flower}{flower}'
+      rule :flower, :@flowers
+      rule :flowers, "🌷", "🌻", "🌼"
+    end
+
+    10.times do
+      expect(grammar.generate).to eq(grammar.generate)
+    end
   end
 end
