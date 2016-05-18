03c2167983933beccfa58c7d574ff1c39ad756b7
Mark Rickerby <me@maetl.net>
2016-05-18 11:15:11 +1000

    Tidy up expression spec to cover multiple cases

--- spec/production/expression_spec.rb
+++ spec/production/expression_spec.rb
@@ -1,10 +1,28 @@
 require 'spec_helper'
 
 describe Calyx::Production::Expression do
-  specify 'construct string formatting production' do
-    nonterminal = double(:nonterminal)
-    allow(nonterminal).to receive(:evaluate).and_return([:atom, 'hello'])
-    rule = Calyx::Production::Expression.new(nonterminal, ['upcase'])
+  let(:production) do
+    double(:production)
+  end
+
+  it 'evaluates a value' do
+    allow(production).to receive(:evaluate).and_return([:atom, 'hello'])
+
+    rule = Calyx::Production::Expression.new(production, [])
+    expect(rule.evaluate).to eq([:expression, 'hello'])
+  end
+
+  it 'evaluates a value with modifier' do
+    allow(production).to receive(:evaluate).and_return([:atom, 'hello'])
+
+    rule = Calyx::Production::Expression.new(production, ['upcase'])
     expect(rule.evaluate).to eq([:expression, 'HELLO'])
+  end
+
+  it 'evaluates a value with modifier chain' do
+    allow(production).to receive(:evaluate).and_return([:atom, 'hello'])
+
+    rule = Calyx::Production::Expression.new(production, ['upcase', 'squeeze'])
+    expect(rule.evaluate).to eq([:expression, 'HELO'])
   end
 end
