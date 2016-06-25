7498c866427388e29c2a286dcd14d99e1e04cd92
Mark Rickerby <me@maetl.net>
2016-06-25 19:52:28 +1000

    Extract load/format test cases to sample files
    This reduces the reliance on mocking File IO which turns out to have
    issues on Travis CI/JRuby.

--- spec/load_spec.rb
+++ spec/load_spec.rb
@@ -1,37 +1,26 @@
 require 'spec_helper'
+require 'psych'
 
 describe "#load" do
+  def sample(filename)
+    "#{__dir__}/format/samples/#{filename}"
+  end
+
   specify 'can load a YAML file properly' do
-      yaml = <<-EOF
-      start: "Hello World"
-      EOF
-    file_name = "example.yml"
-    allow(File).to receive(:read).with(file_name).and_return(yaml)
-    grammar = Calyx::Grammar.load(file_name)
+    grammar = Calyx::Grammar.load(sample('hello.yml'))
     expect(grammar.generate).to eq("Hello World")
   end
 
   specify "can load a JSON file properly" do
-      json = <<-EOF
-      {
-        "start": "Hello World"
-      }
-      EOF
-      file_name = "example.json"
-      allow(File).to receive(:read).with(file_name).and_return(json)
-      grammar = Calyx::Grammar.load(file_name)
-      expect(grammar.generate).to eq("Hello World")
+    grammar = Calyx::Grammar.load(sample('hello.json'))
+    expect(grammar.generate).to eq("Hello World")
   end
 
   specify "raises error if given a file that it cannot parse" do
-    mystery = <<-EOF
-    ? start: "Hello World" ¿
-    EOF
-    file_name = "example.mystery"
-    allow(File).to receive(:read).with(file_name).and_return(mystery)
-    expect do
-      Calyx::Grammar.load(file_name)
-    end.to raise_error('Cannot convert .mystery files.')
+    expect { Calyx::Grammar.load(sample('bad_syntax.yml')) }.to raise_error(Psych::SyntaxError)
   end
 
+  specify "raises error with bad file extension" do
+    expect { Calyx::Grammar.load(sample('bad_extension.bad')) }.to raise_error('Cannot convert .bad files.')
+  end
 end
