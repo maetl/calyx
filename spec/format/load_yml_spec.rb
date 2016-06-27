9792a86f11e480cd7b611f3b19a80cac9e4c5362
Mark Rickerby <me@maetl.net>
2016-06-27 11:53:58 +1000

    Move YAML tests into separate files

--- spec/format/load_yml_spec.rb
+++ spec/format/load_yml_spec.rb
@@ -1,55 +1,31 @@
 require 'spec_helper'
 
 describe "#load_yml" do
+  def sample(filename)
+    File.read("#{__dir__}/samples/#{filename}.yml")
+  end
+
   specify 'generate with recursive rules' do
-    yaml = <<-EOF
-    start: :hello_world
-    hello_world: :statement
-    statement: "Hello World"
-    EOF
-    grammar = Calyx::Format.load_yml(yaml)
+    grammar = Calyx::Format.load_yml(sample('hello_statement'))
     expect(grammar.generate).to eq("Hello World")
   end
 
   specify 'generate with multiple choices' do
-    yaml = <<-EOF
-    start:
-      - :dragon_wins
-      - :hero_wins
-    dragon_wins: "Dragon Wins"
-    hero_wins: "Hero Wins"
-    EOF
-    grammar = Calyx::Format.load_yml(yaml)
+    grammar = Calyx::Format.load_yml(sample('multiple_choices'))
     expect(grammar.generate).to match(/Dragon Wins|Hero Wins/)
   end
 
-  specify 'generate with template expansion' do
-    yaml = <<-EOF
-    start: "{fruit}"
-    fruit:
-      - apple
-      - orange
-    EOF
-    grammar = Calyx::Format.load_yml(yaml)
+  specify 'generate with rule expansion' do
+    grammar = Calyx::Format.load_yml(sample('rule_expansion'))
     expect(grammar.generate).to match(/apple|orange/)
   end
 
   specify 'generate with weighted choices' do
-    yaml = <<-EOF
-    start:
-      - [60%, 0.6]
-      - [40%, 0.4]
-    EOF
-    grammar = Calyx::Format.load_yml(yaml)
+    grammar = Calyx::Format.load_yml(sample('weighted_choices'))
     expect(grammar.generate).to match(/40%|60%/)
   end
 
   specify 'raise error if weighted choices do not sum to 1' do
-    yaml = <<-EOF
-    start:
-      - [90%, 0.9]
-      - [80%, 0.8]
-    EOF
-    expect { Calyx::Format.load_yml(yaml) }.to raise_error('Weights must sum to 1')
+    expect { Calyx::Format.load_yml(sample('bad_weights')) }.to raise_error('Weights must sum to 1')
   end
 end
