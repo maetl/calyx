1760aba63e222a413260674049e374fc9d773d76
Mark Rickerby <me@maetl.net>
2016-06-27 11:09:58 +1000

    Refactor #load test to touch the Format API directly

--- spec/format/load_spec.rb
+++ spec/format/load_spec.rb
@@ -3,24 +3,24 @@ require 'psych'
 
 describe "#load" do
   def sample(filename)
-    "#{__dir__}/format/samples/#{filename}"
+    "#{__dir__}/samples/#{filename}"
   end
 
   specify 'can load a YAML file properly' do
-    grammar = Calyx::Grammar.load(sample('hello.yml'))
+    grammar = Calyx::Format.load(sample('hello.yml'))
     expect(grammar.generate).to eq("Hello World")
   end
 
   specify "can load a JSON file properly" do
-    grammar = Calyx::Grammar.load(sample('hello.json'))
+    grammar = Calyx::Format.load(sample('hello.json'))
     expect(grammar.generate).to eq("Hello World")
   end
 
   specify "raises error if given a file that it cannot parse" do
-    expect { Calyx::Grammar.load(sample('bad_syntax.yml')) }.to raise_error(Psych::SyntaxError)
+    expect { Calyx::Format.load(sample('bad_syntax.yml')) }.to raise_error(Psych::SyntaxError)
   end
 
   specify "raises error with bad file extension" do
-    expect { Calyx::Grammar.load(sample('bad_extension.bad')) }.to raise_error('Cannot convert .bad files.')
+    expect { Calyx::Format.load(sample('bad_extension.bad')) }.to raise_error('Cannot convert .bad files.')
   end
 end
