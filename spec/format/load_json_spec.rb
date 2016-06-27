1b67c67b2f1d265a3885110d9f818e4213609b81
Mark Rickerby <me@maetl.net>
2016-06-27 11:48:12 +1000

    Move JSON tests into separate files

--- spec/format/load_json_spec.rb
+++ spec/format/load_json_spec.rb
@@ -1,46 +1,26 @@
 require 'spec_helper'
 
 describe "#load_json" do
+  def sample(filename)
+    File.read("#{__dir__}/samples/#{filename}.json")
+  end
+
   specify 'generate with recursive rules' do
-    json_text = <<-EOF
-    {
-      "start": "{hello_world}",
-      "hello_world": "{statement}",
-      "statement": "Hello World"
-    }
-    EOF
-    grammar = Calyx::Format.load_json(json_text)
+    grammar = Calyx::Format.load_json(sample('hello_statement'))
     expect(grammar.generate).to eq("Hello World")
   end
 
   specify 'generate with multiple choices' do
-    json_text = <<-EOF
-    {
-      "start": ["{dragon_wins}", "{hero_wins}"],
-      "dragon_wins": "Dragon Wins",
-      "hero_wins": "Hero Wins"
-    }
-    EOF
-    grammar = Calyx::Format.load_json(json_text)
+    grammar = Calyx::Format.load_json(sample('multiple_choices'))
     expect(grammar.generate).to match(/Dragon Wins|Hero Wins/)
   end
 
   specify 'generate with weighted choices' do
-    json_text = <<-EOF
-    {
-      "start": [["60%", 0.6], ["40%", 0.4]]
-    }
-    EOF
-    grammar = Calyx::Format.load_json(json_text)
+    grammar = Calyx::Format.load_json(sample('weighted_choices'))
     expect(grammar.generate).to match(/40%|60%/)
   end
 
   specify 'raise error if weighted choices do not sum to 1' do
-    json_text = <<-EOF
-    {
-      "start": [["90%", 0.9], ["80%", 0.8]]
-    }
-    EOF
-    expect { Calyx::Format.load_json(json_text) }.to raise_error('Weights must sum to 1')
+    expect { Calyx::Format.load_json(sample('bad_weights')) }.to raise_error('Weights must sum to 1')
   end
 end
