c2228bd683a7a2c9a1c5e82bfc57ba05fd9ba0e1
Mark Rickerby <me@maetl.net>
2016-06-28 13:05:31 +1000

    Add test of memoized production rules

---
+++ spec/production/memo_spec.rb
@@ -0,0 +1,8 @@
+describe Calyx::Production::Memo do
+  it 'uses the registry to memoize expansions' do
+    registry = double('registry')
+    allow(registry).to receive(:memoize_expansion).with(:one).and_return('ONE')
+    memo = Calyx::Production::Memo.new(:@one, registry)
+    expect(memo.evaluate).to eq([:one, 'ONE'])
+  end
+end
