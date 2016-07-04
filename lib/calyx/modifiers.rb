64ad5a13ee87cf02192ccdf211738fc017578eca
Mark Rickerby <me@maetl.net>
2016-07-04 21:50:39 +1000

    Add method comments to `Modifiers` source

--- lib/calyx/modifiers.rb
+++ lib/calyx/modifiers.rb
@@ -1,5 +1,15 @@
 module Calyx
   class Modifiers
+    # Transforms an output string by delegating to the given output function.
+    #
+    # If a registered modifier method is not found, then delegate to the given
+    # string function.
+    #
+    # If an invalid modifier function is given, returns the raw input string.
+    #
+    # @param name [Symbol]
+    # @param value [String]
+    # @return [String]
     def transform(name, value)
       if respond_to?(name)
         send(name, value)
