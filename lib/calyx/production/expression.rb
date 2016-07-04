fe7650cd9421c5b009d944215a5bcd0582e791a9
Mark Rickerby <me@maetl.net>
2016-07-04 22:23:57 +1000

    Add method comments to `Production::Expression`

--- lib/calyx/production/expression.rb
+++ lib/calyx/production/expression.rb
@@ -1,12 +1,22 @@
 module Calyx
   module Production
     class Expression
+      # Constructs a node representing a single template substitution.
+      #
+      # @param production [#evaluate]
+      # @param methods [Array]
+      # @param registry [Calyx::Registry]
       def initialize(production, methods, registry)
         @production = production
         @methods = methods.map { |m| m.to_sym }
         @registry = registry
       end
 
+      # Evaluate the expression by expanding the non-terminal to produce a
+      # terminal string, then passing it through the given modifier chain and
+      # returning the transformed result.
+      #
+      # @return [Array]
       def evaluate
         terminal = @production.evaluate.flatten.reject { |o| o.is_a?(Symbol) }.join(''.freeze)
         expression = @methods.reduce(terminal) do |value, method|
