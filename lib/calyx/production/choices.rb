a7c77a26625973b44a18ab24c6709e7d9f3ce939
Mark Rickerby <me@maetl.net>
2016-07-04 22:22:53 +1000

    Add method comments to `Production::Choices` source

--- lib/calyx/production/choices.rb
+++ lib/calyx/production/choices.rb
@@ -1,6 +1,11 @@
 module Calyx
   module Production
     class Choices
+      # Parse a list of productions and return a choice node which is the head
+      # of a syntax tree of child nodes.
+      #
+      # @param productions [Array]
+      # @param registry [Calyx::Registry]
       def self.parse(productions, registry)
         choices = productions.map do |choice|
           if choice.is_a?(String)
@@ -18,10 +23,16 @@ module Calyx
         self.new(choices)
       end
 
+      # Initialize a new choice with a list of child nodes.
+      #
+      # @param collection [Array]
       def initialize(collection)
         @collection = collection
       end
 
+      # Evaluate the choice by randomly picking one of its possible options.
+      #
+      # @return [Array]
       def evaluate
         [:choice, @collection.sample.evaluate]
       end
