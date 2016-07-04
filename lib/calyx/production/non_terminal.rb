dcd8f6e5a6dd92cd00bb5b15d7b8ca2dd0ad24dd
Mark Rickerby <me@maetl.net>
2016-07-04 22:27:17 +1000

    Add method comments to `Production::NonTerminal`

--- lib/calyx/production/non_terminal.rb
+++ lib/calyx/production/non_terminal.rb
@@ -1,11 +1,19 @@
 module Calyx
   module Production
     class NonTerminal
+      # Construct a non-terminal node, given the symbol to lookup and the
+      # registry to look it up in.
+      #
+      # @param symbol [Symbol]
+      # @param registry [Calyx::Registry]
       def initialize(symbol, registry)
         @symbol = symbol.to_sym
         @registry = registry
       end
 
+      # Evaluate the non-terminal, using the registry to handle the expansion.
+      #
+      # @return [Array]
       def evaluate
         [@symbol, @registry.expand(@symbol).evaluate]
       end
