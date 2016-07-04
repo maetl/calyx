4fc7382a5d3f1f15b7f55f18895d1c5d58c3eabe
Mark Rickerby <me@maetl.net>
2016-07-04 22:24:27 +1000

    Add method comments to `Production::Memo` source

--- lib/calyx/production/memo.rb
+++ lib/calyx/production/memo.rb
@@ -3,11 +3,19 @@ module Calyx
     class Memo
       SIGIL = '@'.freeze
 
+      # Construct a memoized rule, given the symbol to lookup and the registry
+      # to look it up in.
+      #
+      # @param symbol [Symbol]
+      # @param registry [Calyx::Registry]
       def initialize(symbol, registry)
         @symbol = symbol.slice(1, symbol.length-1).to_sym
         @registry = registry
       end
 
+      # Evaluate the memo, using the registry to handle the expansion.
+      #
+      # @return [Array]
       def evaluate
         [@symbol, @registry.memoize_expansion(@symbol)]
       end
