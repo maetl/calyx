306624428c9bf3bc6f9db468219ec0ab57522055
Mark Rickerby <me@maetl.net>
2016-07-04 22:35:11 +1000

    Add method comments to `Production::WeightedChoices`

--- lib/calyx/production/weighted_choices.rb
+++ lib/calyx/production/weighted_choices.rb
@@ -1,6 +1,11 @@
 module Calyx
   module Production
     class WeightedChoices
+      # Parse a given list or hash of productions into a syntax tree of weighted
+      # choices.
+      #
+      # @param productions [Array<Array>, Hash<#to_s, Float>]
+      # @param registry [Calyx::Registry]
       def self.parse(productions, registry)
         weights_sum = productions.reduce(0) do |memo, choice|
           memo += choice.last
@@ -19,10 +24,20 @@ module Calyx
         self.new(choices)
       end
 
+      # Initialize a new choice with a list of child nodes.
+      #
+      # @param collection [Array]
       def initialize(collection)
         @collection = collection
       end
 
+      # Evaluate the choice by randomly picking one of its possible options,
+      # balanced according to the given weights.
+      #
+      # The method for selecting weighted probabilities is based on a snippet
+      # of code recommended in the Ruby standard library documentation.
+      #
+      # @return [Array]
       def evaluate
         choice = @collection.max_by do |_, weight|
           rand ** (1.0 / weight)
