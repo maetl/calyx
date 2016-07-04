11bf65128fcebc5daad9705c145eb106849fbebc
Mark Rickerby <me@maetl.net>
2016-07-04 22:29:38 +1000

    Add method comments to `Production::Terminal`

--- lib/calyx/production/terminal.rb
+++ lib/calyx/production/terminal.rb
@@ -1,10 +1,16 @@
 module Calyx
   module Production
     class Terminal
+      # Construct a terminal node with the given value.
+      #
+      # @param atom [#to_s]
       def initialize(atom)
         @atom = atom
       end
 
+      # Evaluate the terminal by returning its identity directly.
+      #
+      # @return [Array]
       def evaluate
         [:atom, @atom]
       end
