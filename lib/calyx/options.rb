a9eb94fe56cf6597237c6dc32f95dd42246a5c4c
Mark Rickerby <me@maetl.net>
2017-09-03 11:46:40 +1000

    Add API docs for options class

--- lib/calyx/options.rb
+++ lib/calyx/options.rb
@@ -1,13 +1,25 @@
 module Calyx
+  # Provides access to configuration options while evaluating a grammar.
   class Options
+    # These options are used by default if not explicitly provided during
+    # initialization of the grammar.
     DEFAULTS = {
       strict: true
     }
 
-    def initialize(opts={})
-      @options = DEFAULTS.merge(opts)
+    # Constructs a new options instance, merging the passed in options with the
+    # defaults.
+    #
+    # @params [Hash, Calyx::Options] options
+    def initialize(options={})
+      @options = DEFAULTS.merge(options)
     end
 
+    # Returns the internal random number generator instance. If a seed or random
+    # instance is not passed-in directly, a new instance of `Random` is
+    # initialized by default.
+    #
+    # @return [Random]
     def rng
       unless @options[:rng]
         @options[:rng] = if @options[:seed]
@@ -20,20 +32,39 @@ module Calyx
       @options[:rng]
     end
 
+    # Returns the next pseudo-random number in the sequence defined by the
+    # internal random number generator state.
+    #
+    # The value returned is a floating point number between 0.0 and 1.0,
+    # including 0.0 and excluding 1.0.
+    #
+    # @return [Float]
     def rand
       rng.rand
     end
 
+    # True if the strict mode option is enabled. This option defines whether or
+    # not to raise an error on missing rules. When set to false, missing rules
+    # are skipped over when the production is concatenated.
+    #
+    # @return [TrueClass, FalseClass]
     def strict?
       @options[:strict]
     end
 
-    def merge(opts)
-      Options.new(@options.merge(opts.to_h))
+    # Merges two instances together and returns a new instance.
+    #
+    # @params [Calyx::Options]
+    # @return [Calyx::Options]
+    def merge(options)
+      Options.new(@options.merge(options.to_h))
     end
 
+    # Serializes instance data to a hash.
+    #
+    # @return [Hash]
     def to_h
-      @options
+      @options.dup
     end
   end
 end
